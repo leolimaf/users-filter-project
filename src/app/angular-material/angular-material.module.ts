631313fbc202851f67c0921390cc0c7e23d2b0b7
Leonardo Lima <leonardolimaf54@gmail.com>
2024-10-01 13:36:22 -0300

    feat: add filter section

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-
+import { MatInputModule } from '@angular/material/input';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  imports: [MatListModule, MatDividerModule],
-  exports: [MatListModule, MatDividerModule],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatListModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [UserDetailsComponent],
-  imports: [AngularMaterialModule],
-  exports: [UserDetailsComponent],
+  declarations: [UserDetailsComponent, FilterComponent],
+  imports: [AngularMaterialModule, FormsModule],
+  exports: [UserDetailsComponent, FilterComponent, FormsModule],
 })
 export class ComponentsModule {}
